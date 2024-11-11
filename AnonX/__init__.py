2dbc9b976fc7f75c9bc5f6941508860ecc4e2b53
Ishu_official <somnathhor12052005@gmail.com>
2024-11-12 03:37:19 +0530

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -3,7 +3,7 @@ from AnonX.core.dir import dirr
 from AnonX.core.git import git
 from AnonX.core.userbot import Userbot
 from AnonX.misc import dbb, heroku, sudo
-from aiohttp import ClientSession
+
 
 from .logging import LOGGER
 
@@ -38,49 +38,3 @@ Resso = RessoAPI()
 SoundCloud = SoundAPI()
 Telegram = TeleAPI()
 
-aiohttpsession = ClientSession()
-import asyncio
-from aiohttp import ClientSession
-
-# Async function jo ClientSession ko initialize karega
-async def create_session():
-    global aiohttpsession
-    aiohttpsession = ClientSession()
-
-# Async function ko run karne ke liye asyncio.run() ka use karenge
-asyncio.run(create_session())
-import asyncio
-from aiohttp import ClientSession
-
-# Global variable to store the aiohttp session
-aiohttpsession = None
-
-# Async function jo ClientSession ko initialize karega
-async def create_session():
-    global aiohttpsession
-    aiohttpsession = ClientSession()
-
-# Code ko async environment mein run karne ke liye isko wrap karte hain
-def initialize_session():
-    asyncio.run(create_session())
-
-# Ab initialize_session ko call karke session start kar sakte hain
-initialize_session()
-
-import asyncio
-from aiohttp import ClientSession
-
-# Global variable to store the aiohttp session
-aiohttpsession = None
-
-# Async function to initialize the ClientSession
-async def create_session():
-    global aiohttpsession
-    aiohttpsession = ClientSession()
-
-# Function to start the async environment and call create_session
-def initialize_session():
-    asyncio.run(create_session())
-
-# Calling initialize_session to set up aiohttpsession
-initialize_session()
