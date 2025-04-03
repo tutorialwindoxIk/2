b8285d44661f5d93abf837038aeb77ff59605455
Ishu_official <somnathhor12052005@gmail.com>
2025-04-04 04:35:55 +0530

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiohttp import ClientSession
-from AnonX.core.bot import AnonXBot
+from AnonX.core.bot import Anon
 from AnonX.core.dir import dirr
 from AnonX.core.git import git
 from AnonX.core.userbot import Userbot
